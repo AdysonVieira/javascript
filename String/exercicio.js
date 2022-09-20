df98335570733c5cc83c08718eead6d3a901773d
Adyson Vieira <contato.adyson@gmail.com>
2022-09-20 15:22:20 -0300

    string exercicio ok

--- String/exercicio.js
+++ String/exercicio.js
@@ -30,9 +30,9 @@ let recebimentoTotal = 0
 transacoes.forEach((transacao) => {
     const valorLimpo = +transacao.valor.replace('R$ ','')
 
-    if(transacao.descricao.startsWith('Taxa') === true) {
+    if(transacao.descricao.startsWith('Taxa')) {
         return taxaTotal += valorLimpo
-    } else if(transacao.descricao.startsWith('Recebimento') === true) {
+    } else if(transacao.descricao.startsWith('Recebimento')) {
         return recebimentoTotal += valorLimpo
     }
 })
@@ -40,18 +40,32 @@ console.log(taxaTotal, recebimentoTotal)
 
 // Retorne uma array com a lista abaixo
 const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const arrayTransportes = transportes.split(';')
+console.log(arrayTransportes)
 
 // Substitua todos os span's por a's
-const html = `<ul>
+let html = `<ul>
                 <li><span>Sobre</span></li>
                 <li><span>Produtos</span></li>
                 <li><span>Contato</span></li>
             </ul>`;
 
+html = html.split('span').join('a')
+console.log(html)
+
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
+console.log(frase.slice(-1))
 
 // Retorne o total de taxas
 const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
 
+let contadorDeTaxa = 0
 
+transacoes2.forEach(transacao => {
+    if (transacao.toLowerCase().trim().includes('taxa')){
+        return contadorDeTaxa++
+    }
+})
+
+console.log(contadorDeTaxa)
