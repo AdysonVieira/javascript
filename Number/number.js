d62e60195b5629ed4d08d2dc385078965842f455
Adyson Vieira <contato.adyson@gmail.com>
2022-09-21 12:17:30 -0300

    exercicio number

--- Number/number.js
+++ Number/number.js
@@ -15,5 +15,33 @@ console.log((99.90).toFixed()) // 100
 // toLocaleString(lang, option)
 
 let valor = 27.09
-valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // R$ 27,09
 console.log(valor)
+
+// Math
+Math.abs(-5.5) // 5.5 | transforma em valores reais
+Math.ceil(5.324) // 6 | transforma em inteiro e sempre arredonda pra cima
+Math.round(5.324) // 5 | transforma em inteiro e arredonda
+Math.floor(5.324) // 5 | transforma em inteiro e sempre arredonda pra baixo
+
+let numero1 = 1
+let numero2 = 2
+let numero3 = 2
+let numero4 = 2
+
+Math.max(numero1, numero2) // 2 | mostra o maior valor da lista
+Math.max(1, 2, 3, 4, 5) // 5 | mostra o maior valor da lista
+Math.min(1, 2, 3, 4, 5) // 1 | mostra o menor valor da lista
+
+Math.random() // retorna um número aleatório
+const aleatorio = Math.random() * 1000 // retorna um numero entre 0 e 1000
+console.log(Math.floor(aleatorio)) // Math.floor para arredondar para baixo e mostrar um número inteiro
+
+// Math.random() * (max - min + 1) + min | Para sortear entre 20 e 40
+console.log(Math.floor(Math.random() * (100 - 001 + 1) + 000))
+
+numero1 = Math.floor(Math.random() * 9)
+numero2 = Math.floor(Math.random() * 9)
+numero3 = Math.floor(Math.random() * 9)
+numero4 = Math.floor(Math.random() * 9)
+console.log(`A sequência ganhadora é ${numero1}${numero2}${numero3}${numero4}`)
