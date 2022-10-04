b6392864a4d904798b83746d73a022796756ef3a
Adyson Vieira <contato.adyson@gmail.com>
2022-10-04 17:55:45 -0300

    iniciando módulos

---
+++ Modules/modules.js
@@ -0,0 +1,10 @@
+// Porque usar?
+
+// ####### Manutenção
+// Dividir o código em diferentes arquivos com funções específicas(módulos) facilita a manutenção
+
+// ####### Compartilhamento
+// O compartilhamento de código com outros projetos é facilitado, pois basta você importar um módulo específico
+
+// ####### Nativo do ES6
+// Ferramentas que permintem dividirmos o código em módulos já existe a bastante tempo. Grunt, Gulp, Webpack, Browserify, Parcel e outras. Mas agora os módulos são Nativos
