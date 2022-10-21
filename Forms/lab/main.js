12f8c9400cafb0e3ad33ae65fab2148958962c2f
Adyson Vieira <contato.adyson@gmail.com>
2022-10-21 20:34:48 -0300

    localstorage

--- Forms/lab/main.js
+++ Forms/lab/main.js
@@ -1,5 +1,6 @@
 // selecionando o formulário e adicionando evento de mudança
-document.getElementById('controles').addEventListener('change', handleChange);
+const controles = document.getElementById('controles');
+controles.addEventListener('change', handleChange);
 
 // selecionando o seletor com classe css e o btn
 const css = document.querySelector('.css');
@@ -38,7 +39,31 @@ const handleStyle = {
     }
 }
 
+// função que acessa o objeto com os métodos com chaves e valores capturados do target
 function handleChange(event) {
-    handleStyle[event.target.name](event.target.value)
+    handleStyle[event.target.name](event.target.value); // executa na tela
+    saveValues(event.target.name, event.target.value); // salva no localStorage
+    showCSS();
 }
+
+// coloca dentro do elemento css os estilos do botão com a propriedade .cssText
+function showCSS() {
+    css.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
+}
+
+// função para salvar os valores no localStorage
+function saveValues(key, value) {
+    localStorage[key] = value;
+}
+
+function setValues() {
+    const keys = Object.keys(localStorage) // chaves salvas no localStorage
+    // para cada chave salva no localStorage
+    keys.forEach(name => {
+        handleStyle[name](localStorage[name].value) // seto no objeto handleStyle 
+        controles.elements[name].value = localStorage[name] // seto no value do input form os valores de localStorage
+    })
+    showCSS()
+}
+setValues();
 
