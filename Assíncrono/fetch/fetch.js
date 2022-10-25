1cb6335173482100fb981fb454e6b6276f78ea2a
Adyson Vieira <contato.adyson@gmail.com>
2022-10-25 11:49:49 -0300

    aulas fetch

--- Assíncrono/fetch/fetch.js
+++ Assíncrono/fetch/fetch.js
@@ -38,32 +38,87 @@
 
 
 
-// Response.blob() | É um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transformamos requisições de imagens, por exemplo. O blob gera um URL único.
-const $div = document.createElement('div');
+// // Response.blob() | É um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transformamos requisições de imagens, por exemplo. O blob gera um URL único.
+// const $div = document.createElement('div');
 
-fetch('./imagem.png')
-.then(response => response.blob())
-.then(imgBlob => {
-    const blobURL = URL.createObjectURL(imgBlob)
-    const img = document.querySelector('img')
-    img.src = blobURL
-    img.width = 200
-})
-
-
+// fetch('./imagem.png')
+// .then(response => response.blob())
+// .then(imgBlob => {
+//     const blobURL = URL.createObjectURL(imgBlob)
+//     const img = document.querySelector('img')
+//     img.src = blobURL
+//     img.width = 200
+// })
 
 
-// Response.clone() | Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o método clone, caso você necessite transformar uma resposta em diferentes valores.
-const $$div = document.createElement('div')
+
+
+// // Response.clone() | Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o método clone, caso você necessite transformar uma resposta em diferentes valores.
+// const $$div = document.createElement('div')
+
+// fetch('https://viacep.com.br/ws/'+ 57442000 +'/json/')
+// .then(response => {
+//     const cloneResponse = response.clone();
+//     response.json().then(json => {
+//         console.log(json)
+//     });
+//     cloneResponse.text().then(text => {
+//         console.log(text)
+//     });
+// })
+
+
+
+
+
+// .headers | São o cabeçalho da requisição. São dados iteráveis, portanto podemos usar o forEach para ver todos eles
 
 fetch('https://viacep.com.br/ws/'+ 57442000 +'/json/')
 .then(response => {
-    const cloneResponse = response.clone();
-    response.json().then(json => {
-        console.log(json)
-    });
-    cloneResponse.text().then(text => {
-        console.log(text)
-    });
+    response.headers.forEach(console.log)
 })
+
+
+
+
+
+// .status e .ok | Retorna o status da requisição, se foi 404, 200, 202. Ok retorna um booleano onde true indica uma requisição bem sucedida e false para uma requisição mau sucedida.
+fetch('https://viacep.com.br/ws/57442000/json/')
+.then(response => {
+    console.log(response.status, response.ok);
+    if(response.status === 404) {
+        console.log('Página não encontrada')
+    } else if(response.status === 200) {
+        console.log('Tudo normal por aqui')
+    }
+})
+
+
+
+
+
+
+// .url e .type | retorna a URL da requisição e o tipo (basic, cors, error, opaque)
+fetch('./doc.txt')
+.then(response => {
+    console.log(response.url); // http://127.0.0.1:5500/Ass%C3%ADncrono/fetch/doc.txt
+    console.log(response.type); // basic | tipo de requisição no próprio servidor
+})
+
+fetch('https://viacep.com.br/ws/57442000/json/')
+.then(response => {
+    console.log(response.url); // https://viacep.com.br/ws/57442000/json/
+    console.log(response.type); // cors | tipo de requisição para outro servidor que é permitida
+})
+
+fetch('https://www.google.com.br')
+.then(response => {
+    console.log(response.status) // erro
+    console.log(response.type) // no-cors
+})
+
+// bacis | tipo de requisição no próprio servidor
+// cors | tipo de requisição para outro servidor que é permitida
+// error | erro de conexão
+// opaque | no-cors, não permite acesso de outros sites
 
